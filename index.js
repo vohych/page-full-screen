b25cbc430119d13de7eeaf49ef63616537e8b121
Volodymyr Pyshnyi <volodymyr.pyshnyi@stairwaysoft.com>
2023-03-04 11:26:48 +0200

    Update

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'Alt') {
+    document.querySelector('.full_screen_page').classList.add('active');
+  }
+})
+window.addEventListener('keyup', (e) => {
+  if (e.key === 'Alt') {
+    document.querySelector('.full_screen_page').classList.remove('active');
+  }
+})
+
 function openFullscreen() {
   const elem = document.querySelector('html');
-  // const elem = document.querySelector('body');
   if (elem.requestFullscreen) {
     elem.requestFullscreen();
   } else if (elem.webkitRequestFullscreen) { /* Safari */
@@ -29,43 +39,44 @@ eventFullScreenElement.innerHTML = 'Full screen';
 
 eventFullScreenElement.addEventListener('click', () => {
   openFullscreen();
-})
-
-
-const eventFullScreenElementExit = document.createElement('span');
-eventFullScreenElementExit.style.color = '#fff';
-eventFullScreenElementExit.style.width = 'max-content';
-eventFullScreenElementExit.style.cursor = 'pointer';
-eventFullScreenElementExit.innerHTML = 'Exit full screen';
-eventFullScreenElementExit.addEventListener('click', () => {
   closeFullscreen();
 })
 
+const createElementStyle = document.createElement('div');
+createElementStyle.innerHTML = `<style>
+.full_screen_page{
+  width : max-content;
+  flex-direction : column;
+  z-index : 99999999999;
+  justify-content : center;
+  align-items : center;
+  border : 1px solid #fff;
+  border-radius : 10px;
+  position : fixed;
+  top : 0;
+  transition : .5s;
+  overflow : hidden;
+  padding : 10px;
+  right : 0;
+  background : #000;
+  display: none;
+}
 
+.full_screen_page.active{
+  display: flex;
+}
+</style>`;
 const createElement = document.createElement('div');
-createElement.style.width = 'max-content';
-// createElement.style.minHeight = '100px';
-createElement.style.display = 'flex';
-createElement.style.flexDirection = 'column';
-createElement.style.zIndex = '99999999999';
-createElement.style.justifyContent = 'center';
-createElement.style.alignItems = 'center';
-createElement.style.border = '1px solid #fff';
-createElement.style.borderRadius = '10px';
-createElement.style.position = 'absolute';
-createElement.style.top = '10px';
-createElement.style.transition = '.5s';
-createElement.style.overflow = 'hidden';
-createElement.style.padding = '10px';
-createElement.style.right = '10px';
-createElement.style.background = '#000';
+createElement.classList.add('full_screen_page');
+
 document.querySelector('body').insertAdjacentElement('afterbegin', createElement);
 
 console.log(createElement)
 
 createElement.insertAdjacentElement('afterbegin', eventFullScreenElement);
-createElement.insertAdjacentElement('afterbegin', eventFullScreenElementExit);
+// createElement.insertAdjacentElement('afterbegin', eventFullScreenElementExit);
+createElement.insertAdjacentElement('afterbegin', createElementStyle);
 
-window.addEventListener('DOMContentLoaded', () => {
-  openFullscreen();
-})
+setTimeout(() => {
+  console.log(document.querySelector('html'))
+}, 1000)
